48c7e0a5d143e41963de491b497df1e26f7a1e68
arrdem <me@arrdem.com>
2015-04-01 15:50:15 -0500

    Update project metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject me.arrdem/guten-tag "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Good tags for a good day!"
+  :url "http://github.com/arrdem/guten-tag"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
