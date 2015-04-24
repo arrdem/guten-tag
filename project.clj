754610de39e51736f4f91a5f44bfdf4e6b39d55d
Reid 'arrdem' McKenzie <me@arrdem.com>
2015-04-24 12:40:23 -0500

    0.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject me.arrdem/guten-tag "0.1.0-SNAPSHOT"
+(defproject me.arrdem/guten-tag "0.1.0"
   :description "Good tags for a good day!"
   :url "http://github.com/arrdem/guten-tag"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/core.match "0.3.0-alpha4"]]}})
