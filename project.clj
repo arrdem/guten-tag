9d372c31dbb2cd2f60e2af642f93c3ff025f3ef7
Reid 'arrdem' McKenzie <reid@factual.com>
2015-06-11 17:04:16 -0700

    Bump to 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject me.arrdem/guten-tag "0.1.0"
+(defproject me.arrdem/guten-tag "0.1.1"
   :description "Good tags for a good day!"
   :url "http://github.com/arrdem/guten-tag"
   :license {:name "Eclipse Public License"
